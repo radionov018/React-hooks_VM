be9baf654400ee8f8c36777eea7849c299dd8a14
Rostislav Radionov <rostislav.radionov018@gmail.com>
2020-05-09 12:17:20 +0300

    hook - useState

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoItem({ title, id, completed }) {
+  const [checked, setChecked] = useState(completed);
+
+  const classesCss = ["todo"];
+
+  if (checked) {
+    classesCss.push("completed");
+  }
+
   return (
-    <li className="todo">
+    <li className={classesCss.join(" ")}>
       <label>
-        <input type="checkbox" defaultChecked={false} />
+        <input
+          type="checkbox"
+          defaultChecked={checked}
+          onChange={() => setChecked(!checked)}
+        />
         <span>{title}</span>
 
         <i className="material-icons red-text">delete</i>
